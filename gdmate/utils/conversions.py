752d9133a2a08dec96cd66ad86dbd4ef25534cd4
Dylan Vasey <dyvasey@gmail.com>
2022-07-18 10:59:52 -0700

    Added utilities subpackage with conversions module

---
+++ gdmate/utils/conversions.py
@@ -0,0 +1,13 @@
+"""
+Unit conversion functions
+"""
+
+def km2m(km):
+    """ Convert km to m """
+    m = km *1000
+    return(m)
+
+def m2km(m):
+    """ Convert m to km """
+    km = m/1000
+    return(km)
