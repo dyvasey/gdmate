8bb5456bea5ddc74364c23a13f48a7ad7749e93e
Dylan Vasey <dyvasey@gmail.com>
2022-07-19 15:07:43 -0700

    Added conversion to SI units

--- gdmate/inputs/flow.py
+++ gdmate/inputs/flow.py
@@ -3,11 +3,12 @@ Module for generating appropriate flow law parameters
 """
 
 class FlowLaw:
-    def __init__(self,material,source,wetness,creep):
+    def __init__(self,material,source,wetness,creep,COH=1000):
         self.material = material
         self.source = source
         self.wetness = wetness
         self.creep = creep
+        self.COH = COH
 
         return
 
@@ -15,40 +16,48 @@ class FlowLaw:
         props = (self.material,self.source,self.wetness,self.creep)
 
         if props == ('olivine','hirth','dry','dislocation'):
-            self.A_raw = 1.1e5 # s^-1 MPa^-n
+            self.A_pub = 1.1e5 # s^-1 MPa^-n um^m COH^-r
             self.n = 3.5
             self.m = 0
-            self.r = None
-            self.E = 530 # kJ/mol
-            self.V = 18 # 10^-6 m^3/mol - note this is actually a range
+            self.r = 0
+            self.E_pub = 530 # kJ/mol
+            self.V_pub = 18 # 10^-6 m^3/mol - note this is actually a range
 
         elif props = ('olivine','hirth','dry','diffusion'):
-            self.A_raw = 1.5e9 # s^-1 MPa^-n
+            self.A_pub = 1.5e9 # s^-1 MPa^-n um^m COH^-r
             self.n = 1
             self.m = 3
-            self.r = None
-            self.E = 375 # kJ/mol
-            self.V = 4 # 10^-6 m^3/mol - note this is actually a range
+            self.r = 0
+            self.E_pub = 375 # kJ/mol
+            self.V_pub = 4 # 10^-6 m^3/mol - note this is actually a range
         
         elif props = ('olivine','hirth','wet','dislocation'):
-            self.A_raw = 1600 # s^-1 MPa^-n
+            self.A_pub = 1600 # s^-1 MPa^-n um^m COH^-r
             self.n = 3.5
             self.m = 0
             self.r = 1.2
-            self.E = 520 # kJ/mol
-            self.V = 22 # 10^-6 m^3/mol
+            self.E_pub = 520 # kJ/mol
+            self.V_pub = 22 # 10^-6 m^3/mol
         
         elif props = 'olivine','hirth','wet','diffusion'):
-            self.A_raw = 2.5e7 # s^-1 MPa^-n
+            self.A_pub = 2.5e7 # s^-1 MPa^-n um^m COH^-r
             self.n = 1
             self.m = 3
             self.r = 1 # actually a range
-            self.E = 375 # kJ/mol
-            self.V = 4 # 10^-6 m^3/mol
+            self.E_pub = 375 # kJ/mol
+            self.V_pub = 4 # 10^-6 m^3/mol
 
         values = (self.A_raw,self.n,self.m,self.r,self.E,self.V)
         return(values)
 
+    def convert2SI(self):
+        self.A_SI = self.A_pub / 1e6^-self.n / 1e6^self.m / self.COH^-self.r # s^-1 Pa^-n m^m COH^-r
+
+        self.E_SI = self.E_pub * 1e3 # j/mol
+
+        self.V_SI = self.V_pub * 1e-6 # m^3/mol
+
+        return(self.A_SI,self.E_SI,self.V_SI)
         
 
 
